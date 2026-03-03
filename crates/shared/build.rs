431d6c6cdac192a6e4b8a5bc10c3ede591f3e0aa
boris <borisyegorov8@gmail.com>
2026-03-03 21:51:43 +0100

    Some cleanup

--- crates/shared/build.rs
+++ crates/shared/build.rs
@@ -1,12 +1,16 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path()?);
-    prost_build::compile_protos(
-        &[
-            "proto/click.proto",
-            "proto/view.proto",
-            "proto/purchase.proto",
-        ],
-        &["proto/"],
-    )?;
+    let out_dir = std::path::Path::new("src/generated");
+    std::fs::create_dir_all(out_dir)?;
+    prost_build::Config::new()
+        .out_dir(out_dir)
+        .compile_protos(
+            &[
+                "proto/click.proto",
+                "proto/view.proto",
+                "proto/purchase.proto",
+            ],
+            &["proto/"],
+        )?;
     Ok(())
 }
--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -1,3 +1,3 @@
 pub mod events {
-    include!(concat!(env!("OUT_DIR"), "/events.rs"));
+    include!("generated/events.rs");
 }
