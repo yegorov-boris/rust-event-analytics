5d3b19ae7f055cd47b0052056956d9b79f22c0e4
boris <borisyegorov8@gmail.com>
2026-03-08 21:53:17 +0100

    Add materialized views

--- init.sql
+++ init.sql
@@ -35,3 +35,61 @@ CREATE TABLE IF NOT EXISTS purchases (
 ) ENGINE = MergeTree()
 PARTITION BY toYYYYMMDD(timestamp)
 ORDER BY (timestamp, product_id);
+
+CREATE TABLE IF NOT EXISTS views_per_minute (
+    minute     DateTime,
+    product_id UInt64,
+    views      UInt64
+) ENGINE = SummingMergeTree()
+ORDER BY (minute, product_id)
+TTL minute + INTERVAL 24 HOUR;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS mv_views
+TO views_per_minute
+AS SELECT
+    toStartOfMinute(timestamp) AS minute,
+    product_id,
+    count()                    AS views
+FROM views
+GROUP BY minute, product_id;
+
+CREATE TABLE IF NOT EXISTS revenue_by_minute (
+    minute        DateTime,
+    revenue_cents UInt64
+) ENGINE = SummingMergeTree()
+ORDER BY (minute)
+TTL minute + INTERVAL 24 HOUR;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS mv_revenue
+TO revenue_by_minute
+AS SELECT
+FROM purchases
+    toStartOfMinute(timestamp)  AS minute,
+    sum(quantity * price_cents) AS revenue
+GROUP BY minute;
+
+CREATE TABLE IF NOT EXISTS conversions_per_minute (
+    minute    DateTime,
+    clicks    UInt64,
+    purchases UInt64
+) ENGINE = SummingMergeTree()
+ORDER BY (minute)
+TTL minute + INTERVAL 24 HOUR;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS mv_conversions_clicks
+TO conversions_per_minute
+AS SELECT
+    toStartOfMinute(timestamp) AS minute,
+    count()                    AS clicks,
+    0                          AS purchases
+FROM clicks
+GROUP BY minute;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS mv_conversions_purchases
+TO conversions_per_minute
+AS SELECT
+    toStartOfMinute(timestamp) AS minute,
+    0                          AS clicks,
+    count()                    AS purchases
+FROM purchases
+GROUP BY minute;
